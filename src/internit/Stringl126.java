8ba95c56c41f6952900163da8a47d144d389ef64
jarvis149 <sankalp1402@gmail.com>
2020-05-28 14:16:49 +0530

    Strings/level1

---
+++ src/internit/Stringl126.java
@@ -0,0 +1,14 @@
+
+package internit;
+
+public class Stringl126 {
+    public static void main(String[] args){
+        String s="JAVA";int i,j;
+        for(i=0;i<4;i++){
+            for(j=0;j<=i;j++){
+                System.out.print(s.charAt(j));
+            }
+            System.out.println();
+        }
+    }
+}
