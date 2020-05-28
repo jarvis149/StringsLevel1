51410ef33272c5162852024852b44acba290b881
jarvis149 <sankalp1402@gmail.com>
2020-05-28 14:08:54 +0530

    Strings/level1

---
+++ src/internit/Stringl114.java
@@ -0,0 +1,14 @@
+package internit;
+import java.util.Scanner;
+public class Stringl114 {
+    public static void main(String[] args){
+        String s="15 august is independence day";
+        System.out.println("The String is: "+s);
+        s=s.replace("15","26");
+        s=s.replace("august","January");
+        s=s.replace("independence","Republic");
+        System.out.println("The above String after Changes is: "+s);
+        
+    }
+    
+}
