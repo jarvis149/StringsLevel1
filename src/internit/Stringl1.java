90671cd2566de63763004fe038726fb51d1c0cf0
jarvis149 <sankalp1402@gmail.com>
2020-05-28 13:52:35 +0530

    Strings/level1

---
+++ src/internit/Stringl1.java
@@ -0,0 +1,14 @@
+
+package internit;
+import java.util.Scanner;
+public class Stringl1 {
+
+   
+    public static void main(String[] args) {
+        Scanner ob= new Scanner(System.in);
+        String s1=ob.nextLine();
+        System.out.println("The sentence entered by the user is: "+ s1);
+        
+    }
+    
+}
